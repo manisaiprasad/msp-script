f8922f5f04fa0c20e5210d44d6e9a5e546a41929
manisaiprasadam <manisaiprasadam@gmail.com>
2020-11-01 23:43:21 +0530

    Evaluator Added

--- src/msp_lang/repl/repl.go
+++ src/msp_lang/repl/repl.go
@@ -4,14 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"msp_lang/evaluator"
 	"msp_lang/lexer"
-	"msp_lang/token"
+	"msp_lang/object"
+	"msp_lang/parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -22,9 +25,40 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+
+const SAD_FACE = `            
+
+███████╗██████╗░██████╗░░█████╗░██████╗░░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
+█████╗░░██████╔╝██████╔╝██║░░██║██████╔╝╚█████╗░
+██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗░╚═══██╗
+███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║██████╔╝
+╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+¯\_(ツ)_/¯
+
+`
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, SAD_FACE)
+	io.WriteString(out, "Woops! We ran into some errors here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
